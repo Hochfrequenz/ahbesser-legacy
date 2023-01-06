868dcfdb086d7b6f4f74ab1ceede623b7346a0d6
konstantin <konstantin.klein@hochfrequenz.de>
2023-01-06 21:07:03 +0100

    Fix oryx build (#9)

--- ahb_viewer/svelte.config.js
+++ ahb_viewer/svelte.config.js
@@ -1,15 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import azure from 'svelte-adapter-azure-swa';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		adapter: adapter()
-	}
+		// adapter: adapter(),
+		adapter: azure()
+	},
 };
 
 export default config;
