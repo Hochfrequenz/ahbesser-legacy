111bb33c02999b3c4160408915c581c0c3a0544c
kevin <kevin.krechan@hochfrequenz.de>
2023-11-02 07:25:56 +0100

    🏷️ Add typehints

--- ahb_viewer/src/store.ts
+++ ahb_viewer/src/store.ts
@@ -1,6 +1,8 @@
 import { writable } from "svelte/store";
+import type { User } from "@auth0/auth0-spa-js";
+
 
 export const isAuthenticated = writable(false);
-export const user = writable({});
+export const user = writable<User | null>(null);
 export const popupOpen = writable(false);
-export const error = writable();
+export const error = writable<Error | null>(null);
