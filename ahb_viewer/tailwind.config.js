d8f736eb74da0d530a441d110733058cd1c26b04
hf-krechan <kevin.krechan@hochfrequenz.de>
2023-11-05 10:01:38 +0100

    🎨 Add HF colors

--- ahb_viewer/tailwind.config.js
+++ ahb_viewer/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#7AAB8A',   // HF primary color
+        secondary: '#669580', // HF shadow color
+        accent: '#E94C74',    // HF accent color 1
+      },
+    },
   },
   plugins: [],
 }
